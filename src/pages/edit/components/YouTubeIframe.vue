fe2c878c97f6b40770339ae7b16da53373d7df95
DeemoHarlos <cces4415@gmail.com>
2021-03-29 09:35:34 +0800

    new page

---
+++ src/pages/edit/components/YouTubeIframe.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {  },
+  setup() {
+    return {}
+  }
+});
+</script>
