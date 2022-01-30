ebbc6fda237b23e71b9d4a4984019e9f60291e36
DeemoHarlos <cces4415@gmail.com>
2022-01-31 02:12:40 +0800

    login to axios

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,20 +1,58 @@
 <template lang="pug">
-  form(action="http://localhost:1233/login" method="post")
+  div
     div
       span: | E-mail
-      input(type="text" name="email" required)
+      input(type="text" name="email" v-model="email" required)
     div
       span: | Password
-      input(type="password" name="password" required)
+      input(type="password" name="password" v-model="password" required)
     div
-      input(type="submit")
+      button(@click="login()"): | LOGIN
+    div: span: | {{ response }}
+    div
+      button(@click="refresh()"): | REFRESH
+    div: span: | {{ secret }}
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
 
 @Component
 export default class extends Vue {
+  email = ''
+  password = ''
+  response = ''
+  secret = ''
+
+  login() {
+    axios
+      .post(
+        'http://localhost:1233/login',
+        {
+          email: this.email,
+          password: this.password,
+        },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        this.response = res ? res.data.success : ''
+      })
+      .catch((err) => {
+        this.response = err.response?.data.error || ''
+      })
+  }
+
+  refresh() {
+    axios
+      .get('http://localhost:1233/secret', { withCredentials: true })
+      .then((res) => {
+        this.secret = res ? res.data.success : ''
+      })
+      .catch((err) => {
+        this.secret = err.response?.data.error || ''
+      })
+  }
 }
 </script>
 
