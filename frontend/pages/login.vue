014054696a643359760593db76164d6afe1c0746
DeemoHarlos <cces4415@gmail.com>
2022-01-30 22:38:40 +0800

    add login frontend, register page to axios

---
+++ frontend/pages/login.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  form(action="http://localhost:1233/login" method="post")
+    div
+      span: | E-mail
+      input(type="text" name="email" required)
+    div
+      span: | Password
+      input(type="password" name="password" required)
+    div
+      input(type="submit")
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+}
+</script>
+
+<style lang="sass" scoped></style>
--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,23 +1,42 @@
 <template lang="pug">
-  form(action="http://localhost:1233/register" method="post")
+  div
     div
       span: | E-mail
-      input(type="text" name="email" required)
+      input(type="text" name="email" v-model="email" required)
     div
       span: | Password
-      input(type="password" name="password" required)
+      input(type="password" name="password" v-model="password" required)
     div
       span: | Password Again
       input(type="password" name="password_again" required)
     div
-      input(type="submit")
+      button(@click="register()"): | REGISTER
+    div: span: | {{ response }}
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
 
 @Component
 export default class extends Vue {
+  email = ''
+  password = ''
+  response = ''
+
+  register() {
+    axios
+      .post('http://localhost:1233/register', {
+        email: this.email,
+        password: this.password,
+      })
+      .then(res => {
+        this.response = res ? res.data.success : ''
+      })
+      .catch((err) => {
+        this.response = err.response?.data.error || ''
+      })
+  }
 }
 </script>
 
