2d51e73f121eff3c02a56d3d9bf10654765f6060
DeemoHarlos <cces4415@gmail.com>
2022-02-02 17:28:33 +0800

    change index to template

---
+++ frontend/pages/template.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  Tutorial
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, useStore, onMounted } from '@nuxtjs/composition-api'
+// import { StoreState } from '@/store'
+
+export default defineComponent({
+  setup() {
+    // const store = useStore() as StoreState
+
+    onMounted(() => {})
+
+    return {}
+  },
+})
+</script>
+
+<style lang="sass" scoped></style>
